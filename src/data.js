44f9b05ddee52ed0b856baefa03e91022c592a91
Hackerdb0002 <88364760+Hackerdb0002@users.noreply.github.com>
2021-09-29 11:03:38 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,13 @@
+import images1 from "./images/images1.jpg";
+import image2 from "./images/images1.jpg";
+import image3 from "./images/images1.jpg";
+import image4 from "./images/images1.jpg";
+import image5 from "./images/images1.jpg";
+import image6 from "./images/images1.jpg";
+import image7 from "./images/images1.jpg";
+import image8 from "./images/images1.jpg";
+import image9 from "./images/images1.jpg";
+
+export const data = [images1, image2, image3, image4, image5, image6, image7, image8, image9];
+export const dog = [images1, image2, image3, image4, image5, image6, image7, image8, image9];
+export const houses = [images1, image2, image3, image4, image5, image6, image7, image8, image9];
