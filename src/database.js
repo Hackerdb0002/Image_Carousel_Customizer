da7ab550b08756d142fa623d3f3d1790b6a6fe7f
Hackerdb0002 <88364760+Hackerdb0002@users.noreply.github.com>
2021-10-01 17:48:38 +0530

    Add files via upload

---
+++ src/database.js
@@ -0,0 +1,17 @@
+const { 
+    createPool
+} = require('mysql');
+
+const pool = createPool({
+    host:"localhost",
+    user:"root",
+    password: "",
+    database:"test",
+    connectionLimit:10
+})
+pool.query("select * from dog", (err, result, fields)=>{
+    if(err){
+        return console.log(err);
+    }
+    return console.log(result);
+})
